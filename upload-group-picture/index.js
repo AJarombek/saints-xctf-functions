3c06b485649fa274eb6a60a51297d0543c64d6c9
ajarombek <ajarombek95@gmail.com>
2021-02-13 21:14:01 -0500

    upload profile picture functional from UI

--- upload-group-picture/index.js
+++ upload-group-picture/index.js
@@ -9,7 +9,7 @@ const { upload } = require('/opt/nodejs/picture');
 exports.handler = async (event) => {
     const env = process.env.ENV;
     const bucket = 'uasset.saintsxctf.com';
-    const key = `${env}/${event.groupId}/${event.fileName}`;
+    const key = `${env}/group/${event.groupId}/${event.fileName}`;
 
     return await upload(bucket, event.base64Image, key);
 };
--- upload-profile-picture/index.js
+++ upload-profile-picture/index.js
@@ -9,7 +9,7 @@ const { upload } = require('/opt/nodejs/picture');
 exports.handler = async (event) => {
     const env = process.env.ENV;
     const bucket = 'uasset.saintsxctf.com';
-    const key = `${env}/${event.username}/${event.fileName}`;
+    const key = `${env}/profile/${event.username}/${event.fileName}`;
 
     return await upload(bucket, event.base64Image, key);
 };
